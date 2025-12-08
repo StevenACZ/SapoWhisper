3bc324ea1190e01a29d876541700527cc1720cf2
steven <scoaila@proton.me>
2025-12-08 17:00:45 -0500

    Initial Commit

---
+++ SapoWhisper/SapoWhisperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SapoWhisperApp.swift
+//  SapoWhisper
+//
+//  Created by Steven on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SapoWhisperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
